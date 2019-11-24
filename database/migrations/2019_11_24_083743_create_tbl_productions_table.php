7a3da53371abd96588cbf3533f544db2dc5a9442
mush60 <mushti8@gmail.com>
2019-11-24 15:53:14 +0700

    add fitur for production controll

---
+++ database/migrations/2019_11_24_083743_create_tbl_productions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTblProductionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_productions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('item_id');
+            $table->integer('jml_item');
+            $table->integer('satuan_id');
+            $table->integer('user_id');
+            $table->date('date');
+            $table->time('time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_productions');
+    }
+}
