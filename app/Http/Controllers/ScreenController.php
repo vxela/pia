f392127a820cdc3a800b185e623739a6b3d8f068
mush60 <mushti8@gmail.com>
2019-12-05 10:59:56 +0700

    screen section

--- app/Http/Controllers/ScreenController.php
+++ app/Http/Controllers/ScreenController.php
@@ -10,13 +10,24 @@ class ScreenController extends Controller
 {
     public function index() {
         $pia = \App\Models\Tbl_item::where('item_name', 'like', 'PIA %')->get();
+        $time = Carbon::now()->format('Y-m-d');
+        $count_cst = DB::table('tbl_stocks')
+                    ->leftJoin('tbl_items', 'tbl_stocks.item_id', '=', 'tbl_items.id')
+                    ->where('stock_date', 'like', $time)
+                    ->where('stock_type', '=', 'in')
+                    ->where('item_name', 'like', 'PIA %')
+                    ->count();
+        if ($count_cst == 0 ) {
+            $time = Carbon::yesterday()->format('Y-m-d');
+        }
         $cst = DB::table('tbl_stocks')
                     ->leftJoin('tbl_items', 'tbl_stocks.item_id', '=', 'tbl_items.id')
-                    ->where('stock_date', 'like', '2019-12-%')
+                    ->where('stock_date', '=', $time)
+                    ->where('stock_type', '=', 'in')
                     ->where('item_name', 'like', 'PIA %')
                     ->get();
 
         // dd($cst);
-        return view('screen.index', ['data_pia' => $pia, 'data_stock' => $cst]);
+        return view('screen.index', ['data_pia' => $pia, 'data_stock' => $cst, 'date' => $time]);
     } 
 }
--- resources/views/screen/index.blade.php
+++ resources/views/screen/index.blade.php
@@ -6,85 +6,42 @@
 
 @section('content')
     <div class="col-12">
-        {{-- <div class="row"> --}}
-            <div class="card mb-4">
-                <div class="card-header bg-white font-weight-bold text-center">
-                    {{Carbon\Carbon::now()->format('F Y')}}
+        @php
+            $ndate = Carbon\Carbon::parse($date)->formatLocalized('%A, %d %B');
+        @endphp
+        <div class="mb-3 text-center">
+            <h2>Produksi {{$ndate}}</h2>
+        </div>
+        <div class="row">
+            @php
+                $bg = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'];
+            @endphp
+            @foreach ($data_stock as $cst)
+                <div class="col-3 mb-3">
+                    <div class="col-md">
+                        <div class="d-flex border">
+                            @php
+                                $nbg = $bg[rand(0,7)];
+                                if($nbg == 'light') {
+                                    $btxt = 'dark';
+                                } else {
+                                    $btxt = 'light';
+                                }
+                            @endphp
+                            <div class="bg-{{$nbg}} text-{{$btxt}} p-4">
+                                <div class="d-flex align-items-center h-100">
+                                    <i class="fa fa-3x fa-fw fa-cubes"></i>
+                                </div>
+                            </div>
+                            <div class="flex-grow-1 bg-white p-4">
+                                <p class="text-uppercase text-secondary mb-0" style="font-weight: bold">{{$cst->item_name}}</p>
+                                <h3 class="font-weight-bold mb-0" style="display: inline;">{{$cst->item_qty.' '}}</h3><small>{{$cst->item_unit}}</small>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <table class="table">
-                        <thead>
-                            <th>#</th>
-                            @foreach ($data_pia as $pia)
-                            <th>{{$pia->item_name}}</th>
-                            @endforeach
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th>Senin</th>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <th>Selasa</th>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <th>Rabu</th>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <th>Kamis</th>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <th>Jumat</th>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <th>Sabtu</th>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        {{-- </div> --}}
+            @endforeach
+        </div>
     </div>
 @endsection
 
