543d99ff275660aef8e44d40766b7522052b62a4
mush60 <mushti8@gmail.com>
2019-12-07 09:38:23 +0700

    setting view for android

--- resources/views/preproduksi/simple_index.blade.php
+++ resources/views/preproduksi/simple_index.blade.php
@@ -17,12 +17,24 @@
         @endif
         <div class="col-12">
             <div class="row">
-                <div class="col-4 bg-primary p-2 mb-2 ml-2">Pia Kacang Hujau</div>
-                <div class="col-4 bg-primary p-2 mb-2 ml-2">Pia Kacang Hujau</div>
-                <div class="col-4 bg-primary p-2 mb-2 ml-2">Pia Kacang Hujau</div>
-                <div class="col-4 bg-primary p-2 mb-2 ml-2">Pia Kacang Hujau</div>
-                <div class="col-4 bg-primary p-2 mb-2 ml-2">Pia Kacang Hujau</div>
-                <div class="col-4 bg-primary p-2 mb-2 ml-2">Pia Kacang Hujau</div>
+                <div class="col-4 bg-primary p-2 mb-2">
+                    <button class="btn btn-success btn-lg">Pia Kacang Hijau</button>
+                </div>
+                <div class="col-4 bg-primary p-2 mb-2">
+                    <button class="btn btn-success btn-lg">Pia Kacang Hijau</button>
+                </div>
+                <div class="col-4 bg-primary p-2 mb-2">
+                    <button class="btn btn-success btn-lg">Pia Kacang Hijau</button>
+                </div>
+                <div class="col-4 bg-primary p-2 mb-2">
+                    <button class="btn btn-success btn-lg">Pia Kacang Hijau</button>
+                </div>
+                <div class="col-4 bg-primary p-2 mb-2">
+                    <button class="btn btn-success btn-lg">Pia Kacang Hijau</button>
+                </div>
+                <div class="col-4 bg-primary p-2 mb-2">
+                    <button class="btn btn-success btn-lg">Pia Kacang Hijau</button>
+                </div>
             </div>
         </div> 
     </div>
