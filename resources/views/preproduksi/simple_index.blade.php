72432d5752fc20016508845a72d1654cb74c857c
mush60 <mushti8@gmail.com>
2019-12-07 10:11:33 +0700

    still setting view

--- resources/views/preproduksi/simple_index.blade.php
+++ resources/views/preproduksi/simple_index.blade.php
@@ -17,22 +17,22 @@
         @endif
         <div class="col-12">
             <div class="row">
-                <div class="col-4 text-center py-2 mb-2">
+                <div class="col-6 text-center py-2 mb-2">
                     <button class="btn btn-success py-5">Kacang Hijau</button>
                 </div>
-                <div class="col-4 text-center py-2 mb-2">
+                <div class="col-6 text-center py-2 mb-2">
                     <button class="btn btn-success py-5">Kacang Hijau</button>
                 </div>
-                <div class="col-4 text-center py-2 mb-2">
+                <div class="col-6 text-center py-2 mb-2">
                     <button class="btn btn-success py-5">Kacang Hijau</button>
                 </div>
-                <div class="col-4 text-center py-2 mb-2">
+                <div class="col-6 text-center py-2 mb-2">
                     <button class="btn btn-success py-5">Kacang Hijau</button>
                 </div>
-                <div class="col-4 text-center py-2 mb-2">
+                <div class="col-6 text-center py-2 mb-2">
                     <button class="btn btn-success py-5">Kacang Hijau</button>
                 </div>
-                <div class="col-4 text-center py-2 mb-2">
+                <div class="col-6 text-center py-2 mb-2">
                     <button class="btn btn-success py-5">Kacang Hijau</button>
                 </div>
             </div>
