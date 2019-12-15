07d5b135c7e92024ee379a2b298e55059cd4c0fa
mush60 <mushti8@gmail.com>
2019-12-15 20:08:11 +0700

    clearing

--- resources/views/screen/index_rt.blade.php
+++ resources/views/screen/index_rt.blade.php
@@ -35,21 +35,7 @@
         $(document).ready(function(){
             
             toastr.options = {
-            "closeButton": true,
-            "debug": false,
-            "newestOnTop": false,
-            "progressBar": false,
-            "positionClass": "toast-top-full-width",
-            "preventDuplicates": false,
-            "onclick": null,
-            "showDuration": "300",
-            "hideDuration": "1000",
-            "timeOut": "5000",
-            "extendedTimeOut": "1000",
-            "showEasing": "swing",
-            "hideEasing": "linear",
-            "showMethod": "fadeIn",
-            "hideMethod": "fadeOut"
+            "closeButton": true, "debug": false, "newestOnTop": false, "progressBar": false, "positionClass": "toast-top-full-width", "preventDuplicates": false, "onclick": null, "showDuration": "300", "hideDuration": "1000", "timeOut": "5000", "extendedTimeOut": "1000", "showEasing": "swing", "hideEasing": "linear", "showMethod": "fadeIn", "hideMethod": "fadeOut"
             };
 
             toastr["success"]("My name is Inigo Montoya. You killed my father. Prepare to die!")
